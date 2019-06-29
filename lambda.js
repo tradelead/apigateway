4fa2cae7943b6d9520519f82fe988c5e1981105c
calebmcelroy <calebmcelroy@gmail.com>
2019-06-28 23:01:40 -0400

    add cors origin support

--- lambda.js
+++ lambda.js
@@ -5,7 +5,22 @@ const serverHandlerPromise = (async () => {
   console.log('Create Schema');
   const schema = await mergeSchemas(JSON.parse(process.env.GRAPHQL_URIS));
   const server = new ApolloServer({ schema });
-  return server.createHandler();
+  const handlerArgs = {};
+
+  if (process.env.CORS_ORIGIN) {
+    handlerArgs.cors = {
+      origin: process.env.CORS_ORIGIN,
+      methods: 'POST',
+      allowedHeaders: [
+        'Content-Type',
+        'Origin',
+        'Accept',
+      ],
+    };
+    console.log(process.env.CORS_ORIGIN, handlerArgs);
+  }
+
+  return server.createHandler(handlerArgs);
 })();
 
 exports.handler = (...args) => {
